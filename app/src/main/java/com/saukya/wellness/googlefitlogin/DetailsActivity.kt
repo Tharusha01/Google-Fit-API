941525c42888b1e802d49b2ae3042ca151d477fb
Tharusha01 <tharushaishan26@gmail.com>
2019-10-01 19:10:19 +0530

    New, Getting Distance and calories from google API feature added

--- app/src/main/java/com/saukya/wellness/googlefitlogin/DetailsActivity.kt
+++ app/src/main/java/com/saukya/wellness/googlefitlogin/DetailsActivity.kt
@@ -1,19 +1,11 @@
 package com.saukya.wellness.googlefitlogin
-
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
-import com.google.android.gms.fitness.FitnessOptions
-import android.util.Log
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.fitness.Fitness
-import com.google.android.gms.fitness.data.DataType
-import com.google.android.gms.fitness.request.DataReadRequest
-import java.util.*
-import java.util.concurrent.TimeUnit
+import android.widget.Toast
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 
 class DetailsActivity : AppCompatActivity() {
@@ -22,76 +14,13 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
-        val back=findViewById<Button>(R.id.btn_back)
-        val googleFit=findViewById<Button>(R.id.btn_googleFit)
+        val back = findViewById<Button>(R.id.btn_back)
 
-        googleFit.setOnClickListener{
 
-            val fitnessOptions = FitnessOptions.builder()
-                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                .build()
 
-            if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
-                GoogleSignIn.requestPermissions(
-                    this, // your activity
-                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
-                    GoogleSignIn.getLastSignedInAccount(this),
-                    fitnessOptions)
-            } else {
-                accessGoogleFit()
-            }
-        }
-
-        back.setOnClickListener{
-            val intent = Intent(this,TestActivity::class.java)
+        back.setOnClickListener {
+            val intent = Intent(this, TestActivity::class.java)
             startActivity(intent)
-        }
-    }
-
-    private fun accessGoogleFit() {
-        val cal = Calendar.getInstance()
-        cal.time = Date()
-        val endTime = cal.timeInMillis
-        cal.add(Calendar.YEAR, -1)
-        val startTime = cal.timeInMillis
-
-
-        val readRequest = DataReadRequest.Builder()
-            .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
-            .bucketByTime(1, TimeUnit.HOURS)
-            .aggregate(DataType.TYPE_LOCATION_SAMPLE, DataType.AGGREGATE_LOCATION_BOUNDING_BOX)
-            .build()
-
-
-
-        GoogleSignIn.getLastSignedInAccount(this)?.let {
-            Fitness.getHistoryClient(this, it)
-                .readData(readRequest)
-                .addOnSuccessListener {
-                    Log.d(LOG_TAG, "onSuccess()")
-                }
-                .addOnFailureListener {
-                        e -> Log.e(LOG_TAG, "onFailure()", e)
-                }
-                .addOnCompleteListener {
-                    Log.d(LOG_TAG, "onComplete()")
-                }
-        }
-    }
-
-    companion object {
-        internal val LOG_TAG: String? = null
-        internal const val GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1
-    }
-
-
-    @SuppressLint("MissingSuperCall")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
-                accessGoogleFit()
-            }
         }
     }
 }
--- app/src/main/java/com/saukya/wellness/googlefitlogin/TestActivity.kt
+++ app/src/main/java/com/saukya/wellness/googlefitlogin/TestActivity.kt
@@ -1,8 +1,9 @@
 package com.saukya.wellness.googlefitlogin
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Color
+import android.content.pm.PackageManager
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -11,13 +12,24 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.fitness.Fitness
 import com.google.android.gms.fitness.FitnessOptions
 import com.google.android.gms.fitness.data.DataType
 import com.google.android.gms.fitness.data.Field
 
+
+
+
+
+@Suppress("IMPLICIT_CAST_TO_ANY")
 class TestActivity : AppCompatActivity() {
+
+    private lateinit var steps: TextView
+    private lateinit var distance: TextView
+    private lateinit var calories: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,28 +37,42 @@ class TestActivity : AppCompatActivity() {
 
         val testBTN = findViewById<Button>(R.id.btn_Test)
 
-        val fitnessOptions = FitnessOptions.builder()
+        steps = findViewById(R.id.tv_StepCount)
+        distance = findViewById(R.id.tv_DistanceCount)
+        calories = findViewById(R.id.tv_CaloriesCount)
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION)
+        }
+
+            val fitnessOptions = FitnessOptions.builder()
                 .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
                 .addDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                .addDataType(DataType.TYPE_DISTANCE_DELTA)
+                .addDataType(DataType.TYPE_DISTANCE_CUMULATIVE)
+                .addDataType(DataType.TYPE_CALORIES_EXPENDED)
                 .build()
-        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
-            GoogleSignIn.requestPermissions(
+            if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+                GoogleSignIn.requestPermissions(
                     this,
                     REQUEST_OAUTH_REQUEST_CODE,
                     GoogleSignIn.getLastSignedInAccount(this),
                     fitnessOptions
-            )
-        } else {
-            subscribe()
-        }
-        initializeLogging()
-        testBTN.setOnClickListener{
+                )
+            } else {
+                subscribe()
+            }
+            initializeLogging()
+            testBTN.setOnClickListener {
 
-            val intent = Intent(this, DetailsActivity::class.java)
-            startActivity(intent)
-        }
-
-
+                val intent = Intent(this, DetailsActivity::class.java)
+                startActivity(intent)
+            }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -66,7 +92,25 @@ class TestActivity : AppCompatActivity() {
             .subscribe(DataType.TYPE_STEP_COUNT_CUMULATIVE)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    Log.i(TAG, "Successfully subscribed!")
+                    Log.i(TAG, "Successfully subscribed for Steps API!")
+                } else {
+                    Log.w(TAG, "There was a problem subscribing.", task.exception)
+                }
+            }
+        Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this)!!)
+            .subscribe(DataType.TYPE_DISTANCE_CUMULATIVE)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.i(TAG, "Successfully subscribed for distance API!")
+                } else {
+                    Log.w(TAG, "There was a problem subscribing.", task.exception)
+                }
+            }
+        Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this)!!)
+            .subscribe(DataType.TYPE_CALORIES_EXPENDED)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.i(TAG, "Successfully subscribed for Calories API!")
                 } else {
                     Log.w(TAG, "There was a problem subscribing.", task.exception)
                 }
@@ -77,8 +121,8 @@ class TestActivity : AppCompatActivity() {
      * Reads the current daily step total, computed from midnight of the current day on the device's
      * current timezone.
      */
-    private fun readData() {
-        var steps = findViewById<TextView>(R.id.tv_StepCount)
+    private fun readDataStep() {
+
         Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this)!!)
             .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
             .addOnSuccessListener { dataSet ->
@@ -88,7 +132,41 @@ class TestActivity : AppCompatActivity() {
                     dataSet.dataPoints[0].getValue(Field.FIELD_STEPS).asInt()).toLong()
                 Toast.makeText(this,"Total steps: $total", Toast.LENGTH_SHORT).show()
                 Log.i(TAG, "Total steps: $total")
-                steps.setText("$total")
+                steps.text = "$total"
+            }
+            .addOnFailureListener { e -> Log.w(TAG, "There was a problem getting the step count.", e) }
+    }
+
+    private fun readDataDistance() {
+
+        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this)!!)
+            .readDailyTotal(DataType.TYPE_DISTANCE_DELTA)
+            .addOnSuccessListener { dataSet ->
+                val total = (if (dataSet.isEmpty)
+                    0
+                else
+                    dataSet.dataPoints[0].getValue(Field.FIELD_DISTANCE).asFloat())
+                Toast.makeText(this,"Total Distance: $total", Toast.LENGTH_SHORT).show()
+                Log.i(TAG, "Total steps: $total")
+                val dis = total.toString() + "m"
+
+                distance.text = dis
+            }
+            .addOnFailureListener { e -> Log.w(TAG, "There was a problem getting the Distance count.", e) }
+    }
+
+    private fun readDataCalories() {
+
+        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this)!!)
+            .readDailyTotal(DataType.TYPE_CALORIES_EXPENDED)
+            .addOnSuccessListener { dataSet ->
+                val total = (if (dataSet.isEmpty)
+                    0
+                else
+                    dataSet.dataPoints[0].getValue(Field.FIELD_CALORIES).asFloat())
+                Toast.makeText(this,"Total Calories: $total", Toast.LENGTH_SHORT).show()
+                Log.i(TAG, "Total Calories: $total")
+                calories.text = "$total"
             }
             .addOnFailureListener { e -> Log.w(TAG, "There was a problem getting the step count.", e) }
     }
@@ -102,7 +180,13 @@ class TestActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
         if (id == R.id.action_read_data) {
-            readData()
+            readDataStep()
+            return true
+        }else if(id == R.id.action_read_distance){
+            readDataDistance()
+            return true
+        }else if(id == R.id.action_read_calories){
+            readDataCalories()
             return true
         }
         return super.onOptionsItemSelected(item)
@@ -133,6 +217,7 @@ class TestActivity : AppCompatActivity() {
 
         val TAG = "StepCounter"
         private val REQUEST_OAUTH_REQUEST_CODE = 0x1001
+        private val MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION = 155
     }
 
 }
